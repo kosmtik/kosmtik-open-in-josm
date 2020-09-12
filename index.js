30189cd45365db271a896911ec08a35db1a8837c
Hidde Wieringa <hidde@hiddewieringa.nl>
2020-09-12 19:26:05 +0200

    Use class syntax
    No idea why it was called `Hash` before :)
    
    Ref https://github.com/kosmtik/kosmtik/pull/319

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-var Hash = function (config) {
-    config.addJS('/node_modules/leaflet-editinosm/Leaflet.EditInOSM.js');
-    config.addCSS('/node_modules/leaflet-editinosm/Leaflet.EditInOSM.css');
-    config.addJS('/open-in-josm.js');
-    config.on('server:init', this.attachRoutes.bind(this));
-};
+class OpenInJOSM {
+    constructor(config) {
+        config.addJS('/node_modules/leaflet-editinosm/Leaflet.EditInOSM.js');
+        config.addCSS('/node_modules/leaflet-editinosm/Leaflet.EditInOSM.css');
+        config.addJS('/open-in-josm.js');
+        config.on('server:init', this.attachRoutes.bind(this));
+    }
 
-Hash.prototype.extendMap = function (req, res) {
-
-    var front = function () {
-        L.K.Map.addInitHook(function () {
-            this.whenReady(function () {
-                var options = {
-                    position: 'topleft',
-                    widget: 'simplebutton',
-                    widgetOptions: {
-                        helpText: 'JOSM',
-                        className: 'kosmtik-open-in-josm'
-                    },
-                    zoomThreshold: 14,
-                    editors: ['josm']
-                };
-                this.openinjosm = (new L.Control.EditInOSM(options)).addTo(this);
+    extendMap(req, res) {
+        var front = function () {
+            L.K.Map.addInitHook(function () {
+                this.whenReady(function () {
+                    var options = {
+                        position: 'topleft',
+                        widget: 'simplebutton',
+                        widgetOptions: {
+                            helpText: 'JOSM',
+                            className: 'kosmtik-open-in-josm'
+                        },
+                        zoomThreshold: 14,
+                        editors: ['josm']
+                    };
+                    this.openinjosm = (new L.Control.EditInOSM(options)).addTo(this);
+                });
             });
-        });
+        };
+        this.pushToFront(res, front);
     };
-    this.pushToFront(res, front);
-};
 
-Hash.prototype.attachRoutes = function (e) {
-    e.server.addRoute('/open-in-josm.js', this.extendMap);
-};
+    attachRoutes(e) {
+        e.server.addRoute('/open-in-josm.js', this.extendMap);
+    };
+}
 
-exports.Plugin = Hash;
+exports = { Plugin: OpenInJOSM };
